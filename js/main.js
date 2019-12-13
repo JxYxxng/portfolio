8c75b5ec71728ce0c87775069b26b472724d00b1
JxYxxng <58196896+JxYxxng@users.noreply.github.com>
2019-12-13 15:52:21 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 $(function(){
 	var a;
-	var b;
 	var t=0;
 	var firstFlag=false;
 	var gnb_down=false;
@@ -22,35 +21,51 @@ $(function(){
 		// w>720 일 때 gnb_fixed show/hide
 		if( gnb_down ){
 			if(t > $(".profile1").offset().top - 70){
-				$("#gnb_fixed").slideDown(500);
+				$("#gnb_fixed").addClass("active");
 			}
 			else{
-				$("#gnb_fixed").slideUp(500);
+				$("#gnb_fixed").removeClass("active");
 			}
 		}
-
 
 		// 변수
 		if(t < $(".profile1").offset().top - 300){
-			b=0;
+			a=0;
 		}
 		else if(t < $(".profile2").offset().top - 300){
-			a=0; b=1;
-		}
-		else if(t < $("#portfolio").offset().top - 300){
 			a=1;
 		}
+		else if(t < $("#portfolio .portfolio1").offset().top - 300){
+			a=2;
+		}
+		else if(t < $("#portfolio .portfolio2").offset().top - 300){
+			a=3;
+		}
+		else if(t < $("#portfolio .portfolio3").offset().top - 300){
+			a=4;
+		}
+		else if(t < $("#portfolio .portfolio4").offset().top - 300){
+			a=5;
+		}
 		else{
-			a=2; b=2;
+			a=6;
 		}
-		// 스크롤 이벤트들
+
+		// 스크롤 이벤트
 		$("#gnb_fixed li, #gnb_mobile li").removeClass("active");
-		$("#gnb_fixed li").eq(b).addClass("active");
-		$("#gnb_mobile li").eq(b).addClass("active");
-		if(a<2) {
-			$("#profile > div").eq(a).addClass("show");
+
+		if(a == 0){
+			$("#gnb_fixed li").eq(a).addClass("active");
+			$("#gnb_mobile li").eq(a).addClass("active");
 		}
-		else if(a<3){
+		else if(a < 3) {
+			$("#profile > div").eq(a-1).addClass("show");
+			$("#gnb_fixed li").eq(1).addClass("active");
+			$("#gnb_mobile li").eq(1).addClass("active");
+		}
+		else{
+			$("#gnb_fixed li").eq(a-1).addClass("active");
+			$("#gnb_mobile li").eq(a-1).addClass("active");
 			$("#portfolio").addClass("show")
 		}
 	});
@@ -65,7 +80,8 @@ $(function(){
 
 		if(w<704 ){
 			gnb_down=false;
-			$("#gnb_fixed").slideUp();
+			// $("#gnb_fixed").slideUp();
+			$("#gnb_fixed").removeClass("active");
 			$(".bg").fadeIn(300);
 		}
 		else{
@@ -81,18 +97,36 @@ $(function(){
 	// *메뉴 클릭시 화면 이동
 	var a2=0;
 	var move=0;
-	$("#gnb > .gnb_inner > ul > li, #gnb_fixed > .gnb_inner > ul > li").click(function(e){
+	$("#gnb > .gnb_inner > ul > li").click(function(e){
 		e.preventDefault();
 
 		a2=$(this).index();
-		move=$("section").eq(a2-1).offset().top;
 
 		if(a2 == 0){
-			$("html").animate({"scrollTop":0},800);
+			move=0;
 		}
 		else {
-			$("html").animate({"scrollTop":move},800);
+			move=$("section").eq(a2-1).offset().top;
 		}
+		$("html").animate({"scrollTop":move},800);
+	});
+
+	// *gnb_fixed
+	$("#gnb_fixed > .gnb_inner > ul > li").click(function(e){
+		e.preventDefault();
+
+		a2=$(this).index();
+
+		if(a2 == 0){
+			move=0;
+		}
+		else if(a2 == 1){
+			move=$("section").eq(a2-1).offset().top;
+		}
+		else{
+			move=$("#portfolio > div").eq(a2-2).offset().top;
+		}
+		$("html").animate({"scrollTop":move},800);
 	});
 
 	// *모바일 메뉴 클릭시 화면 이동
@@ -100,17 +134,30 @@ $(function(){
 		e.preventDefault();
 
 		a2=$(this).index();
-		move=$("section").eq(a2-1).offset().top;
 
 		if(a2 == 0){
-			$("html").animate({"scrollTop":0},800);
+			move=0;
 		}
-		else {
-			$("html").animate({"scrollTop":move},800);
+		else if(a2 == 1){
+			move=$("section").eq(a2-1).offset().top;
 		}
+		else{
+			move=$("#portfolio > div").eq(a2-2).offset().top;
+		}
+		$("html").animate({"scrollTop":move},800);
 		mobile_hide();
 		$(".bg").fadeIn(300);
 	});
+
+	// gnb_fixed 탭
+	$("#gnb_fixed a").hover(
+		function(){
+		$("#gnb_fixed span").stop().fadeIn(300);
+		},
+		function(){
+		$("#gnb_fixed span").stop().fadeOut(300);
+		},
+	);
 
 
 	// *모바일 메뉴 탭
@@ -138,7 +185,6 @@ $(function(){
 		$(".dim").fadeOut(300);
 		$(".tab").removeClass("active");
 	}
-
 
 	var video=document.getElementsByTagName("video");
 	// 포트폴리오 탭
@@ -171,18 +217,16 @@ $(function(){
 			$("#portfolio div[class^=portfolio]").each(function(i){
 				if(i != portNum){
 					$(this).find(".inner").slideUp(800);
-					video[portNum].play();
 				}
 				else{
 					$(this).find(".inner").slideDown(800,function(){
 						$("html").animate({scrollTop:portTab.parents(".content").offset().top},300);
 					});
-					for(var i=0; i<video.length; i++){
-						video[i].pause();
-						video[i].currentTime=0;
-					}
 				}
-
+				for(var i=0; i<video.length; i++){
+					video[i].pause();
+				}
+				video[portNum].play();
 			});
 			$(".port_tab").text("자세히보기");
 			$(this).text("닫기");
